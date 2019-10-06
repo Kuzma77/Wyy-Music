5069a07ae71948fcf5162a1ce5fa576751e21f41
Your Name <Kuzma77>
2019-10-06 18:12:22 +0800

    个人中心详情代码

---
+++ wyyMusic/src/com/bo/servlet/UserPageServlet.java
@@ -0,0 +1,22 @@
+package com.bo.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+/**
+ * @author wl_sun
+ * @ClassName $(NAME)
+ * @Description TODO
+ * @Date 2019/10/6
+ * @Version 1.0
+ **/
+@WebServlet(urlPatterns = "/user")
+public class UserPageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("user.jsp").forward(req, resp);
+    }
+}
